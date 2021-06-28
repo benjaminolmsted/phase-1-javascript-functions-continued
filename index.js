e1b3e8cc8e0fd2ed525a93479d359dfea5612d24
benjamin <benjamin.olmsted@gmail.com>
2021-06-28 13:39:52 -0700

    commit tree 5960459aae8c50b743932447fd15fd67a923a569

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(flair = "*"){
+    return function(msg = "special"){
+       return `You are ${flair}${msg}${flair}!`;
+    }
+}
+
+console.log(wrapAdjective('&&')('the bomb'));
